94fdb14c4b724d6ae76ba9bc284726aa0864bd67
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-02-25 22:17:52 +0700

    Bulk update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { ProductModule } from './microservices/product/product.module';
+import { OrderModule } from './microservices/order/order.module';
+import { CustomerModule } from './microservices/customer/customer.module';
+import { AdminModule } from './microservices/admin/admin.module';
+import { CategoryModule } from './microservices/category/category.module';
+import { PromotionController } from './microservices/promotion/promotion.controller';
+import { PromotionModule } from './microservices/promotion/promotion.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
-  controllers: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    ProductModule,
+    OrderModule,
+    CustomerModule,
+    AdminModule,
+    CategoryModule,
+    PromotionModule,
+  ],
+  controllers: [PromotionController],
   providers: [],
 })
 export class AppModule {}
